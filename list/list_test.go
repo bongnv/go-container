634ddf38c4e1a0bdfa3632f0e45e69f789ad14b6
Bong Nguyen <vanbong@gmail.com>
2023-08-30 08:37:35 +1000

    feat: add test for list

---
+++ list/list_test.go
@@ -0,0 +1,35 @@
+package list_test
+
+import (
+	"testing"
+
+	"github.com/bongnv/go-container/list"
+)
+
+func TestList(t *testing.T) {
+	t.Run("should add and remove nodes properly", func(t *testing.T) {
+		l := list.New[string]()
+		l.PushBack("bong")
+		l.PushBack("c")
+		l.PushFront("a")
+		expectList(t, l, "a", "bong", "c")
+		toRemove := l.Back()
+		l.PushBack("d")
+		l.Remove(toRemove)
+		expectList(t, l, "a", "bong", "d")
+	})
+}
+
+func expectList(t *testing.T, l *list.List[string], elements ...string) {
+	if l.Size() != len(elements) {
+		t.Errorf("Expected size %v but got %v", len(elements), l.Size())
+		return
+	}
+
+	for e, i := l.Front(), 0; e != nil; e, i = e.Next(), i+1 {
+		if e.Value != elements[i] {
+			t.Errorf("Expected %v but got %v", elements[i], e.Value)
+			return
+		}
+	}
+}
