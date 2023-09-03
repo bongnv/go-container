14b9016cd49063106af2ccd191a723c798169ff6
Bong Nguyen <vanbong@gmail.com>
2023-09-03 18:10:23 +1000

    feat: add rbtree

---
+++ rbtree/rbtree.go
@@ -0,0 +1,513 @@
+package rbtree
+
+import (
+	"cmp"
+
+	"github.com/bongnv/go-container/algorithm"
+)
+
+// ItemIterator is a function to iterate through items.
+type ItemIterator[T any] func(i T) bool
+
+// Tree is a Left-Leaning Red-Black (LLRB) implementation of 2-3 trees
+type LLRB[T any] struct {
+	count int
+	root  *Node[T]
+	less  algorithm.LessFunc[T]
+}
+
+// Node represents a node in LLRB.
+type Node[T any] struct {
+	Item        T
+	Left, Right *Node[T] // Pointers to left and right child nodes
+	Black       bool     // If set, the color of the link (incoming from the parent) is black
+	// In the LLRB, new nodes are always red, hence the zero-value for node
+}
+
+// New allocates a new tree
+func New[T cmp.Ordered]() *LLRB[T] {
+	return NewFunc[T](cmp.Less[T])
+}
+
+// NewFunc creates a new LLRB tree using less.
+func NewFunc[T any](less algorithm.LessFunc[T]) *LLRB[T] {
+	return &LLRB[T]{
+		less: less,
+	}
+}
+
+// SetRoot sets the root node of the tree.
+// It is intended to be used by functions that deserialize the tree.
+func (t *LLRB[T]) SetRoot(r *Node[T]) {
+	t.root = r
+}
+
+// Root returns the root node of the tree.
+// It is intended to be used by functions that serialize the tree.
+func (t *LLRB[T]) Root() *Node[T] {
+	return t.root
+}
+
+// Len returns the number of nodes in the tree.
+func (t *LLRB[T]) Len() int { return t.count }
+
+// Has returns true if the tree contains an element whose order is the same as that of key.
+func (t *LLRB[T]) Has(key T) bool {
+	_, found := t.Get(key)
+	return found
+}
+
+// Get retrieves an element from the tree whose order is the same as that of key.
+func (t *LLRB[T]) Get(key T) (item T, present bool) {
+	h := t.root
+	for h != nil {
+		switch {
+		case t.less(key, h.Item):
+			h = h.Left
+		case t.less(h.Item, key):
+			h = h.Right
+		default:
+			return h.Item, true
+		}
+	}
+	return
+}
+
+// Min returns the minimum element in the tree.
+func (t *LLRB[T]) Min() (item T, present bool) {
+	h := t.root
+	if h == nil {
+		return
+	}
+	for h.Left != nil {
+		h = h.Left
+	}
+	return h.Item, true
+}
+
+// Max returns the maximum element in the tree.
+func (t *LLRB[T]) Max() (item T, present bool) {
+	h := t.root
+	if h == nil {
+		return
+	}
+	for h.Right != nil {
+		h = h.Right
+	}
+	return h.Item, true
+}
+
+// Upsert inserts item into the tree. If an existing
+// element has the same order, it is removed from the tree and returned.
+func (t *LLRB[T]) Upsert(item T) (replacedItem T, replaced bool) {
+	t.root, replacedItem, replaced = t.replaceOrInsert(t.root, item)
+	t.root.Black = true
+	if !replaced {
+		t.count++
+	}
+	return replacedItem, replaced
+}
+
+func (t *LLRB[T]) replaceOrInsert(h *Node[T], item T) (node *Node[T], replacedTtem T, replaced bool) {
+	if h == nil {
+		node = newNode[T](item)
+		return
+	}
+
+	h = walkDownRot23(h)
+
+	if t.less(item, h.Item) { // BUG
+		h.Left, replacedTtem, replaced = t.replaceOrInsert(h.Left, item)
+	} else if t.less(h.Item, item) {
+		h.Right, replacedTtem, replaced = t.replaceOrInsert(h.Right, item)
+	} else {
+		replacedTtem, h.Item, replaced = h.Item, item, true
+	}
+
+	h = walkUpRot23(h)
+
+	return h, replacedTtem, replaced
+}
+
+// Insert inserts item into the tree. If an existing
+// element has the same order, both elements remain in the tree.
+func (t *LLRB[T]) Insert(item T) {
+	t.root = t.insertNoReplace(t.root, item)
+	t.root.Black = true
+	t.count++
+}
+
+func (t *LLRB[T]) insertNoReplace(h *Node[T], item T) *Node[T] {
+	if h == nil {
+		return newNode(item)
+	}
+
+	h = walkDownRot23(h)
+
+	if t.less(item, h.Item) {
+		h.Left = t.insertNoReplace(h.Left, item)
+	} else {
+		h.Right = t.insertNoReplace(h.Right, item)
+	}
+
+	return walkUpRot23(h)
+}
+
+// Rotation driver routines for 2-3 algorithm
+
+func walkDownRot23[T any](h *Node[T]) *Node[T] { return h }
+
+func walkUpRot23[T any](h *Node[T]) *Node[T] {
+	if isRed(h.Right) && !isRed(h.Left) {
+		h = rotateLeft(h)
+	}
+
+	if isRed(h.Left) && isRed(h.Left.Left) {
+		h = rotateRight(h)
+	}
+
+	if isRed(h.Left) && isRed(h.Right) {
+		flip(h)
+	}
+
+	return h
+}
+
+// Rotation driver routines for 2-3-4 algorithm
+
+func walkDownRot234[T any](h *Node[T]) *Node[T] {
+	if isRed(h.Left) && isRed(h.Right) {
+		flip(h)
+	}
+
+	return h
+}
+
+func walkUpRot234[T any](h *Node[T]) *Node[T] {
+	if isRed(h.Right) && !isRed(h.Left) {
+		h = rotateLeft(h)
+	}
+
+	if isRed(h.Left) && isRed(h.Left.Left) {
+		h = rotateRight(h)
+	}
+
+	return h
+}
+
+// DeleteMin deletes the minimum element in the tree and returns the
+// deleted item or nil otherwise.
+func (t *LLRB[T]) DeleteMin() (deletedItem T, deleted bool) {
+	t.root, deletedItem, deleted = deleteMin(t.root)
+	if t.root != nil {
+		t.root.Black = true
+	}
+	if deleted {
+		t.count--
+	}
+	return deletedItem, deleted
+}
+
+// deleteMin code for LLRB 2-3 trees
+func deleteMin[T any](h *Node[T]) (node *Node[T], deletedItem T, deleted bool) {
+	if h == nil {
+		return nil, deletedItem, false
+	}
+	if h.Left == nil {
+		return nil, h.Item, true
+	}
+
+	if !isRed(h.Left) && !isRed(h.Left.Left) {
+		h = moveRedLeft(h)
+	}
+
+	h.Left, deletedItem, deleted = deleteMin(h.Left)
+
+	return fixUp(h), deletedItem, deleted
+}
+
+// DeleteMax deletes the maximum element in the tree and returns
+// the deleted item or nil otherwise
+func (t *LLRB[T]) DeleteMax() (deletedItem T, deleted bool) {
+	t.root, deletedItem, deleted = deleteMax(t.root)
+	if t.root != nil {
+		t.root.Black = true
+	}
+	if deleted {
+		t.count--
+	}
+	return deletedItem, deleted
+}
+
+func deleteMax[T any](h *Node[T]) (node *Node[T], deletedItem T, deleted bool) {
+	if h == nil {
+		return nil, deletedItem, false
+	}
+	if isRed(h.Left) {
+		h = rotateRight(h)
+	}
+	if h.Right == nil {
+		return nil, h.Item, true
+	}
+	if !isRed(h.Right) && !isRed(h.Right.Left) {
+		h = moveRedRight(h)
+	}
+	h.Right, deletedItem, deleted = deleteMax(h.Right)
+
+	return fixUp(h), deletedItem, deleted
+}
+
+// Delete deletes an item from the tree whose key equals key.
+// The deleted item is return, otherwise nil is returned.
+func (t *LLRB[T]) Delete(key T) (deletedItem T, deleted bool) {
+	t.root, deletedItem, deleted = t.delete(t.root, key)
+	if t.root != nil {
+		t.root.Black = true
+	}
+	if deleted {
+		t.count--
+	}
+	return deletedItem, deleted
+}
+
+func (t *LLRB[T]) delete(h *Node[T], item T) (node *Node[T], deletedItem T, deleted bool) {
+	if h == nil {
+		return nil, deletedItem, false
+	}
+	if t.less(item, h.Item) {
+		if h.Left == nil { // item not present. Nothing to delete
+			return h, deletedItem, false
+		}
+		if !isRed(h.Left) && !isRed(h.Left.Left) {
+			h = moveRedLeft(h)
+		}
+		h.Left, deletedItem, deleted = t.delete(h.Left, item)
+	} else {
+		if isRed(h.Left) {
+			h = rotateRight(h)
+		}
+		// If @item equals @h.Item and no right children at @h
+		if !t.less(h.Item, item) && h.Right == nil {
+			return nil, h.Item, true
+		}
+		// PETAR: Added 'h.Right != nil' below
+		if h.Right != nil && !isRed(h.Right) && !isRed(h.Right.Left) {
+			h = moveRedRight(h)
+		}
+		// If @item equals @h.Item, and (from above) 'h.Right != nil'
+		if !t.less(h.Item, item) {
+			var subDeleted T
+			h.Right, subDeleted, deleted = deleteMin(h.Right)
+			deletedItem, h.Item = h.Item, subDeleted
+		} else { // Else, @item is bigger than @h.Item
+			h.Right, deletedItem, deleted = t.delete(h.Right, item)
+		}
+	}
+
+	return fixUp(h), deletedItem, deleted
+}
+
+// Internal node manipulation routines
+
+func newNode[T any](item T) *Node[T] { return &Node[T]{Item: item} }
+
+func isRed[T any](h *Node[T]) bool {
+	if h == nil {
+		return false
+	}
+	return !h.Black
+}
+
+func rotateLeft[T any](h *Node[T]) *Node[T] {
+	x := h.Right
+	if x.Black {
+		panic("rotating a black link")
+	}
+	h.Right = x.Left
+	x.Left = h
+	x.Black = h.Black
+	h.Black = false
+	return x
+}
+
+func rotateRight[T any](h *Node[T]) *Node[T] {
+	x := h.Left
+	if x.Black {
+		panic("rotating a black link")
+	}
+	h.Left = x.Right
+	x.Right = h
+	x.Black = h.Black
+	h.Black = false
+	return x
+}
+
+// REQUIRE: Left and Right children must be present
+func flip[T any](h *Node[T]) {
+	h.Black = !h.Black
+	h.Left.Black = !h.Left.Black
+	h.Right.Black = !h.Right.Black
+}
+
+// REQUIRE: Left and Right children must be present
+func moveRedLeft[T any](h *Node[T]) *Node[T] {
+	flip(h)
+	if isRed(h.Right.Left) {
+		h.Right = rotateRight(h.Right)
+		h = rotateLeft(h)
+		flip(h)
+	}
+	return h
+}
+
+// REQUIRE: Left and Right children must be present
+func moveRedRight[T any](h *Node[T]) *Node[T] {
+	flip(h)
+	if isRed(h.Left.Left) {
+		h = rotateRight(h)
+		flip(h)
+	}
+	return h
+}
+
+func fixUp[T any](h *Node[T]) *Node[T] {
+	if isRed(h.Right) {
+		h = rotateLeft(h)
+	}
+
+	if isRed(h.Left) && isRed(h.Left.Left) {
+		h = rotateRight(h)
+	}
+
+	if isRed(h.Left) && isRed(h.Right) {
+		flip(h)
+	}
+
+	return h
+}
+
+func (t *LLRB[T]) AscendRange(greaterOrEqual, lessThan T, iterator ItemIterator[T]) {
+	t.ascendRange(t.root, greaterOrEqual, lessThan, iterator)
+}
+
+func (t *LLRB[T]) ascendRange(h *Node[T], inf, sup T, iterator ItemIterator[T]) bool {
+	if h == nil {
+		return true
+	}
+	if !t.less(h.Item, sup) {
+		return t.ascendRange(h.Left, inf, sup, iterator)
+	}
+	if t.less(h.Item, inf) {
+		return t.ascendRange(h.Right, inf, sup, iterator)
+	}
+
+	if !t.ascendRange(h.Left, inf, sup, iterator) {
+		return false
+	}
+	if !iterator(h.Item) {
+		return false
+	}
+	return t.ascendRange(h.Right, inf, sup, iterator)
+}
+
+// AscendGreaterOrEqual will call iterator once for each element greater or equal to
+// pivot in ascending order. It will stop whenever the iterator returns false.
+func (t *LLRB[T]) AscendGreaterOrEqual(pivot T, iterator ItemIterator[T]) {
+	t.ascendGreaterOrEqual(t.root, pivot, iterator)
+}
+
+func (t *LLRB[T]) ascendGreaterOrEqual(h *Node[T], pivot T, iterator ItemIterator[T]) bool {
+	if h == nil {
+		return true
+	}
+	if !t.less(h.Item, pivot) {
+		if !t.ascendGreaterOrEqual(h.Left, pivot, iterator) {
+			return false
+		}
+		if !iterator(h.Item) {
+			return false
+		}
+	}
+	return t.ascendGreaterOrEqual(h.Right, pivot, iterator)
+}
+
+// AscendLessThan will call iterator once for each element lower than
+// pivot in ascending order. It will stop whenever the iterator returns false.
+func (t *LLRB[T]) AscendLessThan(pivot T, iterator ItemIterator[T]) {
+	t.ascendLessThan(t.root, pivot, iterator)
+}
+
+func (t *LLRB[T]) ascendLessThan(h *Node[T], pivot T, iterator ItemIterator[T]) bool {
+	if h == nil {
+		return true
+	}
+	if !t.ascendLessThan(h.Left, pivot, iterator) {
+		return false
+	}
+	if t.less(h.Item, pivot) {
+		if !iterator(h.Item) {
+			return false
+		}
+		return t.ascendLessThan(h.Right, pivot, iterator)
+	}
+	return true
+}
+
+// DescendLessOrEqual will call iterator once for each element less than the
+// pivot in descending order. It will stop whenever the iterator returns false.
+func (t *LLRB[T]) DescendLessOrEqual(pivot T, iterator ItemIterator[T]) {
+	t.descendLessOrEqual(t.root, pivot, iterator)
+}
+
+func (t *LLRB[T]) descendLessOrEqual(h *Node[T], pivot T, iterator ItemIterator[T]) bool {
+	if h == nil {
+		return true
+	}
+	if t.less(h.Item, pivot) || !t.less(pivot, h.Item) {
+		if !t.descendLessOrEqual(h.Right, pivot, iterator) {
+			return false
+		}
+		if !iterator(h.Item) {
+			return false
+		}
+	}
+	return t.descendLessOrEqual(h.Left, pivot, iterator)
+}
+
+// Scan will call iterator once for each element in ascending order.
+// It will stop whenever the iterator returns false.
+func (t *LLRB[T]) Scan(iterator ItemIterator[T]) {
+	t.ascend(t.root, iterator)
+}
+
+func (t *LLRB[T]) ascend(h *Node[T], iterator ItemIterator[T]) bool {
+	if h == nil {
+		return true
+	}
+	if !t.ascend(h.Left, iterator) {
+		return false
+	}
+	if !iterator(h.Item) {
+		return false
+	}
+	return t.ascend(h.Right, iterator)
+}
+
+// ReverseScan will call iterator once for each element in descending order.
+// It will stop whenever the iterator returns false.
+func (t *LLRB[T]) ReverseScan(iterator ItemIterator[T]) {
+	t.descend(t.root, iterator)
+}
+
+func (t *LLRB[T]) descend(h *Node[T], iterator ItemIterator[T]) bool {
+	if h == nil {
+		return true
+	}
+	if !t.descend(h.Right, iterator) {
+		return false
+	}
+	if !iterator(h.Item) {
+		return false
+	}
+	return t.descend(h.Left, iterator)
+}
---
+++ rbtree/rbtree_test.go
@@ -0,0 +1,280 @@
+package rbtree_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/bongnv/go-container/rbtree"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCases(t *testing.T) {
+	tree := rbtree.New[int]()
+	tree.Upsert(1)
+	tree.Upsert(1)
+	if tree.Len() != 1 {
+		t.Errorf("expecting len 1")
+	}
+	if !tree.Has(1) {
+		t.Errorf("expecting to find key=1")
+	}
+
+	tree.Delete(1)
+	if tree.Len() != 0 {
+		t.Errorf("expecting len 0")
+	}
+	if tree.Has(1) {
+		t.Errorf("not expecting to find key=1")
+	}
+
+	tree.Delete(1)
+	if tree.Len() != 0 {
+		t.Errorf("expecting len 0")
+	}
+	if tree.Has(1) {
+		t.Errorf("not expecting to find key=1")
+	}
+}
+
+func TestReverseInsertOrder(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 100
+	for i := 0; i < n; i++ {
+		tree.Upsert(n - i)
+	}
+	i := 0
+	tree.AscendGreaterOrEqual(0, func(item int) bool {
+		i++
+		if item != i {
+			t.Errorf("bad order: got %d, expect %d", item, i)
+		}
+		return true
+	})
+}
+
+func TestRange(t *testing.T) {
+	tree := rbtree.New[string]()
+	order := []string{
+		"ab", "aba", "abc", "a", "aa", "aaa", "b", "a-", "a!",
+	}
+	for _, i := range order {
+		tree.Upsert(i)
+	}
+	k := 0
+	tree.AscendRange("ab", "ac", func(item string) bool {
+		if k > 3 {
+			t.Fatalf("returned more items than expected")
+		}
+		i1 := order[k]
+		i2 := item
+		if i1 != i2 {
+			t.Errorf("expecting %s, got %s", i1, i2)
+		}
+		k++
+		return true
+	})
+}
+
+func TestRandomInsertOrder(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 1000
+	perm := rand.Perm(n)
+	for i := 0; i < n; i++ {
+		tree.Upsert(perm[i])
+	}
+	j := 0
+	tree.AscendGreaterOrEqual(0, func(item int) bool {
+		if item != j {
+			t.Fatalf("bad order")
+		}
+		j++
+		return true
+	})
+}
+
+func TestRandomReplace(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 100
+	perm := rand.Perm(n)
+	for i := 0; i < n; i++ {
+		tree.Upsert(perm[i])
+	}
+	perm = rand.Perm(n)
+	for i := 0; i < n; i++ {
+		if replacedItem, replaced := tree.Upsert(perm[i]); !replaced || replacedItem != perm[i] {
+			t.Errorf("error replacing")
+		}
+	}
+}
+
+func TestRandomInsertSequentialDelete(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 1000
+	perm := rand.Perm(n)
+	for i := 0; i < n; i++ {
+		tree.Upsert(perm[i])
+	}
+	for i := 0; i < n; i++ {
+		tree.Delete(i)
+	}
+}
+
+func TestRandomInsertDeleteNonExistent(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 100
+	perm := rand.Perm(n)
+	for i := 0; i < n; i++ {
+		tree.Upsert(perm[i])
+	}
+	if _, deleted := tree.Delete(200); deleted {
+		t.Errorf("deleted non-existent item")
+	}
+	if _, deleted := tree.Delete(-2); deleted {
+		t.Errorf("deleted non-existent item")
+	}
+	for i := 0; i < n; i++ {
+		if u, deleted := tree.Delete(i); !deleted || u != i {
+			t.Errorf("delete failed")
+		}
+	}
+	if _, deleted := tree.Delete(200); deleted {
+		t.Errorf("deleted non-existent item")
+	}
+	if _, deleted := tree.Delete(-2); deleted {
+		t.Errorf("deleted non-existent item")
+	}
+}
+
+func TestRandomInsertPartialDeleteOrder(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 100
+	perm := rand.Perm(n)
+	for i := 0; i < n; i++ {
+		tree.Upsert(perm[i])
+	}
+	for i := 1; i < n-1; i++ {
+		tree.Delete(i)
+	}
+	j := 0
+	tree.AscendGreaterOrEqual(0, func(item int) bool {
+		switch j {
+		case 0:
+			if item != 0 {
+				t.Errorf("expecting 0")
+			}
+		case 1:
+			if item != n-1 {
+				t.Errorf("expecting %d", n-1)
+			}
+		}
+		j++
+		return true
+	})
+}
+
+func TestInsertNoReplace(t *testing.T) {
+	tree := rbtree.New[int]()
+	n := 1000
+	for q := 0; q < 2; q++ {
+		perm := rand.Perm(n)
+		for i := 0; i < n; i++ {
+			tree.Insert(perm[i])
+		}
+	}
+	j := 0
+	tree.AscendGreaterOrEqual(0, func(item int) bool {
+		if item != j/2 {
+			t.Fatalf("bad order")
+		}
+		j++
+		return true
+	})
+}
+
+func TestLLRB(t *testing.T) {
+	testCases := map[string]struct {
+		scenario      func(t *rbtree.LLRB[int])
+		expectedOrder []int
+	}{
+		"should be ordered properly": {
+			scenario: func(t *rbtree.LLRB[int]) {
+				t.Insert(1)
+				t.Insert(0)
+				t.Insert(2)
+				t.Insert(2)
+				t.Insert(4)
+			},
+			expectedOrder: []int{0, 1, 2, 2, 4},
+		},
+		"should be ordered properly after deleting": {
+			scenario: func(t *rbtree.LLRB[int]) {
+				t.Insert(1)
+				t.Insert(0)
+				t.Insert(2)
+				t.Insert(4)
+				t.Delete(2)
+			},
+			expectedOrder: []int{0, 1, 4},
+		},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			rbt := rbtree.New[int]()
+			tc.scenario(rbt)
+			allItems := make([]int, 0, rbt.Len())
+			rbt.Scan(func(i int) bool {
+				allItems = append(allItems, i)
+				return true
+			})
+			if diff := cmp.Diff(allItems, tc.expectedOrder); diff != "" {
+				t.Errorf("unexpected order (+got, -wanted): %v", diff)
+			}
+		})
+	}
+}
+
+func TestLLRB_ReverseScan(t *testing.T) {
+	testCases := map[string]struct {
+		scenario      func(t *rbtree.LLRB[int])
+		expectedOrder []int
+	}{
+		"should be ordered properly": {
+			scenario: func(t *rbtree.LLRB[int]) {
+				t.Insert(1)
+				t.Insert(0)
+				t.Insert(2)
+				t.Insert(2)
+				t.Insert(4)
+			},
+			expectedOrder: []int{4, 2, 2, 1, 0},
+		},
+		"should be ordered properly after deleting": {
+			scenario: func(t *rbtree.LLRB[int]) {
+				t.Insert(1)
+				t.Insert(0)
+				t.Insert(2)
+				t.Insert(4)
+				t.Delete(2)
+			},
+			expectedOrder: []int{4, 1, 0},
+		},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			rbt := rbtree.New[int]()
+			tc.scenario(rbt)
+			allItems := make([]int, 0, rbt.Len())
+			rbt.ReverseScan(func(i int) bool {
+				allItems = append(allItems, i)
+				return true
+			})
+			if diff := cmp.Diff(allItems, tc.expectedOrder); diff != "" {
+				t.Errorf("unexpected order (+got, -wanted): %v", diff)
+			}
+		})
+	}
+}
